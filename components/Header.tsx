d2c5080898de1635b9f75a9a7af3c32af757e210
Pratyush <oggycandle786@gmail.com>
2025-06-05 20:03:42 +0530

    Adding header

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,68 +33,70 @@ const Header = () => {
           <Image src={"/logo.png"} alt="no-log" height={40} width={200} />
         </Link>
         <div className="flex justify-center gap-6">
-          <div>
-            <Link href={"/orders"}>
-              <Button
-                variant={"outline"}
-                className="h-10 hidden md:block lg:block"
-              >
-                <div className="flex justify-between">
-                  <SendToBack className="mr-2 mt-0.5"/>
-                  Your Orders
-                </div>
-              </Button>
-            </Link>
-          </div>
-          <div>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Button className="h-10" variant="outline">
-                  <ShoppingCart className="h-4 w-4 mr-2" />
-                  <span>Shop</span>
-                  <ChevronDown className="h-4 w-4 ml-2" />
+          <SignedIn>
+            <div>
+              <Link href={"/orders"}>
+                <Button
+                  variant={"outline"}
+                  className="h-10 hidden md:block lg:block"
+                >
+                  <div className="flex justify-between">
+                    <SendToBack className="mr-2 mt-0.5" />
+                    Your Orders
+                  </div>
                 </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>Shopping Options</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="cursor-pointer">
-                  <ShoppingCart className="h-4 w-4 mr-2" />
-                  <span>My Cart</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer">
-                  <Heart className="h-4 w-4 mr-2" />
-                  <span>Wishlist</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer">
-                  <Package className="h-4 w-4 mr-2" />
-                  <span>Track Orders</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer">
-                  <Tag className="h-4 w-4 mr-2" />
-                  <span>Deals & Offers</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuLabel>Account</DropdownMenuLabel>
-                <DropdownMenuItem className="cursor-pointer">
-                  <User className="h-4 w-4 mr-2" />
-                  <span>Profile</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer">
-                  <CreditCard className="h-4 w-4 mr-2" />
-                  <span>Payment Methods</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer">
-                  <Bell className="h-4 w-4 mr-2" />
-                  <span>Notifications</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer">
-                  <Gift className="h-4 w-4 mr-2" />
-                  <span>Rewards</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
+              </Link>
+            </div>
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button className="h-10" variant="outline">
+                    <ShoppingCart className="h-4 w-4 mr-2" />
+                    <span>Shop</span>
+                    <ChevronDown className="h-4 w-4 ml-2" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>Shopping Options</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem className="cursor-pointer">
+                    <ShoppingCart className="h-4 w-4 mr-2" />
+                    <span>My Cart</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Heart className="h-4 w-4 mr-2" />
+                    <span>Wishlist</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Package className="h-4 w-4 mr-2" />
+                    <span>Track Orders</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Tag className="h-4 w-4 mr-2" />
+                    <span>Deals & Offers</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuLabel>Account</DropdownMenuLabel>
+                  <DropdownMenuItem className="cursor-pointer">
+                    <User className="h-4 w-4 mr-2" />
+                    <span>Profile</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer">
+                    <CreditCard className="h-4 w-4 mr-2" />
+                    <span>Payment Methods</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Bell className="h-4 w-4 mr-2" />
+                    <span>Notifications</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Gift className="h-4 w-4 mr-2" />
+                    <span>Rewards</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </SignedIn>
           <div>
             <SignedOut>
               <Link href={"/explore"} className="mr-3">
